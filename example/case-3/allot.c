a5134352bf51e62d0a986bf15b7d25f5c72ddf91
diwang <diwang83963911@gamil.com>
2023-02-08 15:21:37 +0800

    add case4

--- example/case-3/allot.c
+++ example/case-3/allot.c
@@ -9,6 +9,17 @@ int main(int argc, char const *argv[])
     printf("thanks your input %d\n", length);
     int *p;
     p = (int *)malloc(length * sizeof(int));
+    for (int i = 0; i < length; i++)
+    {
+        p[i] = i;
+        p[i + 1] = i + 1;
+    }
+    for (int i = 0; i < length; i++)
+    {
+        printf("%d ", p[i + 1]);
+    }
+
+    printf("%d ", p[99]);
     free(p);
     return 0;
 }
