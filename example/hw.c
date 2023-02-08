70a2a9de8368ab54041fbf10876c1967f70d7a27
diwang <diwang83963911@gamil.com>
2023-02-08 10:58:38 +0800

    add example

--- example/hw.c
+++ example/hw.c
@@ -5,8 +5,11 @@
 void hello_world(){
     printf("%s,%s\n",HELLO_NAME,WORLD_NAME); 
     printf("%s",HELLO_NAME);
-    char *f[7] ="asdsad";
+    char f[7] ="asdsad";
     printf("%s",f);
+    char temp;
+    scanf("%s",&temp);
+    printf("%s",&temp);
 }
 
 
